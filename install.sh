e118ce0c2420cfb524b0cf0ca1859ed018232551
phanindra srungavarapu <pahninsd@gmail.com>
2015-07-24 18:36:27 +0530

    Changed installation to perform selftest Changed installation to use systemd instead of upstart

--- install.sh
+++ install.sh
@@ -19,7 +19,8 @@ sudo mkdir -p "$JOSH_CONFIG_ROOT"
 sudo mkdir -p "$JOSH_LOG_ROOT"
 sudo chmod a+w "$JOSH_LOG_ROOT"
 sudo chmod a+w "$JOSH_HOSTS_ROOT"
-
+# Stop josh if its running already
+sudo service josh stop > /dev/null 2>&1 || true
 # If josh is already installed, remove it first.
 sudo rm -rf "$JOSH_INSTALL_ROOT"
 
@@ -34,8 +35,13 @@ sudo cp -R "$TMP_ROOT" "$JOSH_INSTALL_ROOT"
 sudo cp ext/libnss_josh.so.2 /lib
 sudo sed -i -r -e '/\bjosh\b/ !s/^hosts:(.+)\bdns\b/hosts:\1josh dns/' /etc/nsswitch.conf
 
-# install upstart service
-cat sysconfigs/upstart.conf | m4 -D__JOSHD_BIN__=$JOSHD_BIN | sudo tee /etc/init/josh.conf > /dev/null 
+if [ -d "/usr/lib/systemd/system" ]; then
+    # systemd is present normally
+    sudo bash -c 'cat sysconfigs/systemd.service > /usr/lib/systemd/system/josh.service'
+else
+    # systemd is present but in a different directory
+    sudo bash -c 'cat sysconfigs/systemd.service > /lib/systemd/system/josh.service'
+fi
 
 # install configuration, only if not already present
 JOSH_CONFIG_FILE_PATH="$JOSH_CONFIG_ROOT/config.json"
@@ -45,7 +51,7 @@ JOSH_CONFIG_FILE_PATH="$JOSH_CONFIG_ROOT/config.json"
 
 echo "*** Starting the josh server..."
 sudo service josh restart
-
+VERSION=$(cat package.json | grep version | tr -d ", ")
 # Show a message about where to go for help.
 
 function print_troubleshooting_instructions() {
@@ -63,7 +69,7 @@ function print_troubleshooting_instructions() {
 # source the configuration and use it to run a self-test.
 
 # disabled for now
-# CONFIG=$(node "$JOSHD_BIN" --print-config 2>/dev/null || true)
+CONFIG=$(node "$JOSHD_BIN" --print-config 2>/dev/null || true)
 
 if [[ -n "$CONFIG" ]]; then
     eval "$CONFIG"
@@ -72,29 +78,7 @@ if [[ -n "$CONFIG" ]]; then
     # Check to see if the server is running at all.
     function check_status() {
         sleep 1
-        curl -sH host:pow "localhost:$JOSH_HTTP_PORT/status.json" | grep -c "$VERSION" >/dev/null
-    }
-
-    # Attempt to connect to Pow via each configured domain. If a
-    # domain is inaccessible, try to force a reload of OS X's
-    # network configuration.
-    function check_domains() {
-        for domain in ${JOSH_DOMAINS//,/$IFS}; do
-            echo | nc "${domain}." "$JOSH_DST_PORT" 2>/dev/null || return 1
-        done
-    }
-
-    # Use networksetup(8) to create a temporary network location,
-    # switch to it, switch back to the original location, then
-    # delete the temporary location. This forces reloading of the
-    # system network configuration.
-    function reload_network_configuration() {
-        echo "*** Reloading system network configuration..."
-        local location=$(networksetup -getcurrentlocation)
-        networksetup -createlocation "pow$$" >/dev/null 2>&1
-        networksetup -switchtolocation "pow$$" >/dev/null 2>&1
-        networksetup -switchtolocation "$location" >/dev/null 2>&1
-        networksetup -deletelocation "pow$$" >/dev/null 2>&1
+        curl -sH host:pow "localhost/status.json" | grep -c $VERSION >/dev/null
     }
 
     # Try twice to connect to Pow. Bail if it doesn't work.
@@ -104,16 +88,6 @@ if [[ -n "$CONFIG" ]]; then
         exit 1
     }
 
-    # Try resolving and connecting to each configured domain. If
-    # it doesn't work, reload the network configuration and try
-    # again. Bail if it fails the second time.
-    check_domains || {
-        { reload_network_configuration && check_domains; } || {
-            echo "!!! Couldn't resolve configured domains ($JOSH_DOMAINS)"
-            print_troubleshooting_instructions
-            exit 1
-        }
-    }
 fi
 
 
